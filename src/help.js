89518b5f7b64f47f43d504c7385e91fcbe4cbe97
Ronaldo03042002 <77816080+Ronaldo03042002@users.noreply.github.com>
2021-01-24 13:41:09 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,177 @@
+
+const help = (prefix) => { 
+	return `                 
+Criador do Bot:Ronaldo Silva
+Tel:wa.me/+557196834782
+
+Menu de comando↓
+╔═════════════════
+║ Comandos listados, espero que goste.
+╠═════════════════
+║   Exclusivo para admin
+║╭────────────────
+║├> ${prefix}welcome [0/1]
+║├> ${prefix}bv [0/1]
+║├> ${prefix}bemvindo [0/1]
+║├> ${prefix}boasvindas [0/1]
+║├──> Desativar/Ativa boas vindas
+║├────────────────
+║├> ${prefix}add 55xxx
+║├> ${prefix}adicionar 55xxx
+║├──> Add membro
+║├────────────────
+║├> ${prefix}kick @membro
+║├> ${prefix}ban @membro
+║├> ${prefix}remover @membro
+║├──> Remover membro
+║├────────────────
+║├> ${prefix}promote @membro
+║├> ${prefix}promover @membro
+║├──> Promover a admin
+║├────────────────
+║├> ${prefix}demote @adm
+║├> ${prefix}promover @adm
+║├──> Rebaixar a membro
+║├────────────────
+║├> ${prefix}grup abrir/fechar
+║├> ${prefix}group abrir/fechar
+║├> ${prefix}grupo abrir/fechar
+║├──> Fechar e abrir o grupo
+║├────────────────
+║├> ${prefix}setname seu texto
+║├> ${prefix}mudarnome seu texto
+║├──> Mudar nome do grupo
+║├────────────────
+║├> ${prefix}setdesc seu texto
+║├> ${prefix}mudardesc seu texto
+║├──> Mudar descrição
+║├────────────────
+║├> ${prefix}mudarfoto
+║├──> Mudar foto do grupo
+║├────────────────
+║├> ${prefix}marcar
+║├──> Marcação
+║├────────────────
+║├> ${prefix}marcar2
+║├──> Marcação 2
+║├────────────────
+║├> ${prefix}marcar3
+║├──> Marcação
+║├────────────────
+║├> Download
+║├────────────────
+║├> ${prefix}tiktokstalk Nick da conta
+║├──> Stalk de tiktok
+║├────────────────
+║├> ${prefix}fototiktok
+║├──> Foto do tiktok
+║├────────────────
+║├> ${prefix}ytmp4 link
+║├> ${prefix}ytvideo link
+║├> ${prefix}ytbuscar link
+║├> ${prefix}ytbaixa link
+║├──> Baixa vídeo do YouTube
+║├────────────────
+║├>Mídia (Não funciona)
+║├────────────────
+║├> ${prefix}sticker
+║├> ${prefix}stiker
+║├> ${prefix}fig
+║├> ${prefix}figurinha
+║├──> 
+║├────────────────
+║├> ${prefix}tts seu texto
+║├> ${prefix}gtts seu texto
+║├> ${prefix}audio seu texto
+║├──> Converter Texto em áudio
+║├────────────────
+║├> ${prefix}toimg marca fig
+║├> ${prefix}converter marca fig
+║├──> Converter figurinha em foto
+║├────────────────
+║├> ${prefix}texto da foto
+║├> ${prefix}ocr
+║├──> Pega texto de foto
+║├────────────────
+║├> ${prefix}wait marca foto
+║├> ${prefix}pesquisar marca foto
+║├──> Pesquisa por anime 
+║├────────────────
+║├> ${prefix}listaadms
+║├> ${prefix}listadmins
+║├> ${prefix}adms
+║├──> Lista dos adms
+║├────────────────
+║├> ${prefix}cachorro
+║├> ${prefix}auau
+║├> ${prefix}dog
+║├──> Cão fofo aleatório
+║├────────────────
+║├> ${prefix}pokemon
+║├──> Pokemon aleatório
+║├────────────────
+║├> ${prefix}logo3d seu texto
+║├──> Logo com seu texto
+║├────────────────
+║├> ${prefix}logoph seu|texto
+║├──> Logo com seu texto
+║├────────────────
+║├> ${prefix}info
+║├──> Informações do bot
+║├────────────────
+║├> ${prefix}listabloq
+║├──> Números bloqueados
+║├────────────────
+║├> ${prefix}lofi
+║├──> Foto única de lofi
+║├────────────────
+║├> ${prefix}map 0000-0000 
+║├> ${prefix} ${prefix}mapa 0000-0000 
+║├──> Pega foto do local
+║├────────────────
+║├>Comandos do criador
+║├────────────────
+║├> ${prefix}hidetag
+║├──> Esconde tag
+║├────────────────
+║├> ${prefix}clearall
+║├> ${prefix}limpar
+║├──> Limpa whats do bot
+║├────────────────
+║├> ${prefix}bloquear @membro
+║├──> Autobloq
+║├────────────────
+║├> ${prefix}desbloquear
+║├──> Autodesbloq
+║├────────────────
+║├> ${prefix}kitar
+║├──> Bot Sair do grupo
+║├────────────────
+║├> ${prefix}bc
+║├──> Transmissão
+║├────────────────
+║├> ${prefix}clonar
+║├> ${prefix}clone
+║├──> Clonar foto
+║├────────────────
+║├>  Suporte
+║├────────────────
+║├> ${prefix}bug
+║├> ${prefix}reportar
+║├> ${prefix}reportarbug
+║├──> Reportar bug ao criador
+║├────────────────
+║├> ${prefix}sigla nova sigla
+║├> ${prefix}setprefix
+║├──> Troca sigla de comandos
+║└────────────────
+╚═════════════════
+
+
+`
+}
+exports.help = help
+
+
+
+  
