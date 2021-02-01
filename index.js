a63d996ad54475a36e1fb8d7305c819e8e5fffad
Ronaldo03042002 <77816080+Ronaldo03042002@users.noreply.github.com>
2021-02-01 11:38:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ client.on('group-participants-update', async (anu) => {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `*Oiee @${num.split('@')[0]}\ seja bem vindo(a) ao grupo *${mdata.subject}* *Siga as regras e evite ser banido do grupo, espero que faça muitas amizades por aqui*`
+				teks = `*Oiee @${num.split('@')[0]}\ seja bem vindo(a) ao grupo ${mdata.subject}* *Siga as regras e evite ser banido do grupo, espero que faça muitas amizades por aqui*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
